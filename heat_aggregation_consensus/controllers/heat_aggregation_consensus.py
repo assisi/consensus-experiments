ba786bc57db9432534df78118c0613b39faff20a
marcelo_ijk <marcelo.ijk@gmail.com>
2016-09-01 20:10:34 +0200

    Current version tested with CASUs only. Seems to be working for simple use-cases

--- heat_aggregation_consensus/controllers/heat_aggregation_consensus.py
+++ heat_aggregation_consensus/controllers/heat_aggregation_consensus.py
@@ -29,29 +29,31 @@ class ConsensusController(Thread):
         self.numbees = [0]
         self.nb_buf_len = 5
         self.ir_thresholds = {}
-        self.ir_thresholds['casu-001'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-002'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-003'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-004'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-005'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-006'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-007'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-008'] = [1,1,1,1,1,1]
-        self.ir_thresholds['casu-009'] = [1,1,1,1,1,1]
+        self.ir_thresholds['casu-001'] = [13000, 10500, 11000, 10500, 10000, 9500]
+        self.ir_thresholds['casu-002'] = [10500, 9000, 13000, 12500, 10000, 10000]
+        self.ir_thresholds['casu-003'] = [8000, 18000, 19500, 15500, 13500, 12000]
+        self.ir_thresholds['casu-004'] = [12000, 16500, 19500, 15000, 14000, 13000]
+        self.ir_thresholds['casu-005'] = [15000, 11500, 14000, 25000, 16500, 14500]
+        self.ir_thresholds['casu-006'] = [16500, 10500, 17000, 13000, 11000, 11500]
+        self.ir_thresholds['casu-007'] = [11000, 10500, 16500, 11500, 13500, 12500]
+        self.ir_thresholds['casu-008'] = [11000, 10500, 14000, 13000, 13000, 11000]
+        self.ir_thresholds['casu-009'] = [11000, 10000, 14000, 16000, 13500, 11500]
 
     def update(self):
 
         casu_id = self.consensus.casu_id
-
-        np.set_printoptions(precision=3)    
+ 
         # Hack for testing
         #numbees_fake = [6,3,0,0,0,0,0,0,0]
         self.update_numbees_estimate()
         numbees = sum(self.numbees)/float(len(self.numbees))
-        print(self.casu.name(),numbees)
+        #print(self.casu.name(),numbees)
 
         # Compute one step of the algorithm
         self.consensus.step(numbees,0.1)
+
+        # Set temperature reference
+        self.casu.set_temp(self.consensus.t_ref[casu_id-1])
 
         # Communicate with neighbors
         for nbg in self.casu._Casu__neighbors:
@@ -77,16 +79,6 @@ class ConsensusController(Thread):
      
                     self.consensus.zeta[-1][int(casu_sender[-3:]) -1] = deepcopy(rec_nbg_zeta)
                     self.consensus.t_ref[int(casu_sender[-3:])-1] = rec_nbg_temp     
-                    
-                    #print('NEW',self.casu.name(), nbg, self.consensus.t_ref) 
-                    
-#                    if (self.casu.name() == 'casu-001'):
-#                        print(self.casu.name(), nbg, int(casu_sender[-3:]), np.array(rec_nbg_zeta), np.array(self.consensus.zeta[-1]))
-
-#['%.3f' % z for z in self.consensus.zeta[-1][nbg-1]])
-#        if (self.casu.name() == 'casu-001') or (self.casu.name() == 'casu-002') or (self.casu.name() == 'casu-004'):
-#        #if (self.casu.name() == 'casu-001'):
-#            print(self.casu.name(), np.array(self.consensus.zeta[-1]), 'T_ref', self.consensus.t_ref)
 
         print(self.casu.name(),['%.3f' % z for z in self.consensus.zeta[-1][self.consensus.casu_id-1]], 'T_ref', self.consensus.t_ref)
     
@@ -105,7 +97,7 @@ class ConsensusController(Thread):
         """
         Bee density estimator.
         """
-        self.numbees.append(sum([x>t for (x,t) in zip(self.casu.get_ir_raw_readings(casu.ARRAY),
+        self.numbees.append(sum([x>t for (x,t) in zip(self.casu.get_ir_raw_value(casu.ARRAY),
                                                       self.ir_thresholds[self.casu.name()])]))
         if len(self.numbees) > self.nb_buf_len:
             self.numbees.pop(0)
